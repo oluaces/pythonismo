cc5578b7243d3109c69b591afa79a3dca492cf2f
oluaces <oluaces@gmail.com>
2017-06-06 09:50:18 +0200

    para python 3.x
    Puse () en los print

--- hello.py
+++ hello.py
@@ -5,10 +5,11 @@ Created on Wed Jun 17 13:21:31 2015
 @author: oluaces
 """
 
-print 'Hello, GitHub!'
+print('Hello, GitHub!')
 
 # print writing on Atom
 i=0
 while i<10:
-    print 'Dentro del bucle (%d)'%i
+    print('Dentro del bucle (%d)'%i)
     i = i + 1
+
